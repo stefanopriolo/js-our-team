e7a6b1bd3cc37da0b6fd2d6e6510e906139ea52c
stefanopriolo <stefanojpriolo@gmail.com>
2023-07-15 17:21:34 +0200

    objects

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const memberList = [
+  {
+    userName: "Wayne Barnett",
+    userRole: "Founder & CEO",
+    userImg: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    userName: "Angela Caroll",
+    userRole: "Chief Editor",
+    userImg: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    userName: "Walter Gordon",
+    userRole: "Office Manager",
+    userImg: "walter-gordon-office-manager.jpg",
+  },
+  {
+    userName: "Angela Lopez",
+    userRole: "Social Media Manager",
+    userImg: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    userName: "Scott Estrada",
+    userRole: "Developer",
+    userImg: "scott-estrada-developer.jpg",
+  },
+  {
+    userName: "Barbara Ramos",
+    userRole: "Graphic Designer",
+    userImg: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+console.log(memberList);
